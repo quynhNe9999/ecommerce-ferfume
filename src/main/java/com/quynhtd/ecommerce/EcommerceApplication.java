be43a9d4ca623f1c1ffc9b18accd2ee82155b1d1
DESKTOP-O58ED60\tdquy <quynhtd@oneempower.com>
2025-12-17 16:39:10 +0700

    log host server local

--- src/main/java/com/quynhtd/ecommerce/EcommerceApplication.java
+++ src/main/java/com/quynhtd/ecommerce/EcommerceApplication.java
@@ -1,12 +1,23 @@
 package com.quynhtd.ecommerce;
 
+import com.quynhtd.ecommerce.utils.LogUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
     public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(EcommerceApplication.class);
+        Map<String, Object> defProperties = new HashMap<>();
+        defProperties.put("spring.profiles.default", "dev");
+        app.setDefaultProperties(defProperties);
+        Environment env = app.run(args).getEnvironment();
+        LogUtils.logApplicationStartup(env);
         SpringApplication.run(EcommerceApplication.class, args);
     }
 
---
+++ src/main/java/com/quynhtd/ecommerce/utils/LogUtils.java
@@ -0,0 +1,43 @@
+package com.quynhtd.ecommerce.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+
+public class LogUtils {
+  private static final Logger logger = LoggerFactory.getLogger(LogUtils.class);
+    public static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+          logger.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+      logger.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+}
